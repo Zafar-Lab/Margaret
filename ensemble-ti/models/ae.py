269f93bd652a4f696210077a38555b7c71bd7cf3
Kushagra Pandey <kpandeyce008@gmail.com>
2021-02-11 01:06:48 +0530

    Added sparse autoencoders

--- ensemble-ti/models/ae.py
+++ ensemble-ti/models/ae.py
@@ -96,6 +96,55 @@ class DAE(nn.Module):
         return out
 
 
+class SparseAE(nn.Module):
+    # Implementation of the Sparse Autoencoder
+    def __init__(self, infeatures, code_size=10, noise_std=1.0):
+        super(SparseAE, self).__init__()
+        self.code_size = code_size
+        self.infeatures = infeatures
+        self.relu = nn.ReLU()
+        self.noise_std = noise_std
+
+        # Encoder architecture
+        self.enc_fc1 = nn.Linear(self.infeatures, 128)
+        self.enc_bn1 = nn.BatchNorm1d(128)
+        self.enc_fc2 = nn.Linear(128, 64)
+        self.enc_bn2 = nn.BatchNorm1d(64)
+        self.enc_fc3 = nn.Linear(64, self.code_size)
+        self.enc_bn3 = nn.BatchNorm1d(self.code_size)
+
+        # Decoder Architecture
+        self.dec_fc1 = nn.Linear(self.code_size, 64)
+        self.dec_bn1 = nn.BatchNorm1d(64)
+        self.dec_fc2 = nn.Linear(64, 128)
+        self.dec_bn2 = nn.BatchNorm1d(128)
+        self.dec_fc3 = nn.Linear(128, self.infeatures)
+        self.dec_bn3 = nn.BatchNorm1d(self.infeatures)
+
+    def encode(self, x):
+        x = self.relu(self.enc_bn1(self.enc_fc1(x)))
+        x = self.relu(self.enc_bn2(self.enc_fc2(x)))
+        x = self.relu(self.enc_bn3(self.enc_fc3(x)))
+        return x
+
+    def decode(self, z):
+        x = self.relu(self.dec_bn1(self.dec_fc1(z)))
+        x = self.relu(self.dec_bn2(self.dec_fc2(x)))
+        output = self.relu(self.dec_bn3(self.dec_fc3(x)))
+        return output
+
+    def forward(self, x):
+        # Add some noise to the input
+        x = x + self.noise_std * torch.randn_like(x)
+
+        # Encoder
+        z = self.encode(x)
+
+        # Decoder
+        out = self.decode(z)
+        return z, out
+
+
 class VAE(nn.Module):
     # Implementation of a Variational Autoencoder
     def __init__(self, infeatures, code_size=10):

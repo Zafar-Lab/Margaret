431b567a2e68de5e5488cbe668e3c16d61d84cc1
Kushagra Pandey <kpandeyce008@gmail.com>
2021-03-07 17:48:27 +0530

    Added gitignote

--- ensemble-ti/test.py
+++ ensemble-ti/test.py
@@ -1,29 +1,32 @@
 import matplotlib.pyplot as plt
 import numpy as np
+# import scanpy as sc
+# import scanpy.external as sce
+# import torch
+# import torchvision
+# import torchvision.transforms as T
+# import scvi
+
+# from sklearn.manifold import TSNE
+# # from dca.api import dca
+
+# from datasets.np import NpDataset
+# # from models.vae import VAE
+# # from models.dae import DAE
+# from utils.preprocess import preprocess_recipe, run_pca
+
 import scanpy as sc
-import scanpy.external as sce
-import torch
-import torchvision
-import torchvision.transforms as T
-import scvi
 
-from sklearn.manifold import TSNE
-from dca.api import dca
+from utils.plot import plot_gt_milestone_network
 
-from datasets.np import NpDataset
-from models.vae import VAE
-from models.dae import DAE
-from utils.preprocess import preprocess_recipe, run_pca
-
-
-random_seed = 0
-np.random.seed(random_seed)
-torch.manual_seed(random_seed)
+# random_seed = 0
+# np.random.seed(random_seed)
+# torch.manual_seed(random_seed)
 
 
 # Load data
-data_path = '/home/lexent/ensemble-ti/ensemble-ti/data/marrow_sample_scseq_counts.csv'
-adata = sc.read(data_path, first_column_names=True)
+data_path = '/home/lexent/Desktop/dyntoy_disconnected_gen_1.h5ad'
+adata = sc.read(data_path)
 
 # adata = anndata.read_h5ad(path_to_anndata)
 # scvi.data.setup_anndata(adata)
@@ -51,7 +54,7 @@ adata = sc.read(data_path, first_column_names=True)
 # X_pca, _, n_comps = run_pca(preprocessed_data, use_hvg=use_hvg)
 # print(f'PCA computed: {X_pca.shape}')
 
-dca(adata, mode='latent')
+# dca(adata, mode='latent')
 
 # # Perform MAGIC Imputation
 # print('Resolving Dropouts. Magic Imputation')
@@ -80,6 +83,9 @@ dca(adata, mode='latent')
 
 # embeddings = np.array(embeddings)
 
-X_embedded = TSNE(n_components=2, perplexity=150).fit_transform(adata.obsm["X_scVI"])
-plt.scatter(embeddings[:, 0], embeddings[:, 1])
+# X_embedded = TSNE(n_components=2, perplexity=150).fit_transform(adata.obsm["X_scVI"])
+# plt.scatter(embeddings[:, 0], embeddings[:, 1])
+# plt.show()
+
+plot_gt_milestone_network(adata)
 plt.show()

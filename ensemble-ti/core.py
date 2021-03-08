9384706203fb7f7e69fb0d6f9e0f3fc5f9bacc7d
Kushagra Pandey <kpandeyce008@gmail.com>
2021-03-08 13:49:09 +0530

    Added encapsulated command for metti

---
+++ ensemble-ti/core.py
@@ -0,0 +1,92 @@
+import networkx as nx
+import numpy as np
+import random
+import torch
+import warnings
+
+from sklearn.neighbors import NearestNeighbors
+
+from models.ti.connectivity import compute_directed_cluster_connectivity, compute_undirected_cluster_connectivity
+from models.ti.graph import compute_gt_milestone_network, compute_connectivity_graph, compute_trajectory_graph
+from models.ti.pseudotime import compute_pseudotime
+from train_metric import train_metric_learner
+from utils.plot import generate_plot_embeddings
+from utils.util import get_start_cell_cluster_id
+
+
+def seed_everything(seed=0):
+    # NOTE: Uses the implementation from Pytorch Lightning
+    """Function that sets seed for pseudo-random number generators  in:
+        pytorch, numpy, python.random
+    """
+    max_seed_value = np.iinfo(np.uint32).max
+    min_seed_value = np.iinfo(np.uint32).min
+
+    if (seed > max_seed_value) or (seed < min_seed_value):
+        raise ValueError(f"{seed} is not in bounds, \
+            numpy accepts from {min_seed_value} to {max_seed_value}"
+        )
+
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    return seed
+
+
+def run_metti(ad, 
+    n_episodes=10, n_metric_epochs=10, use_rep='X_pca', code_size=10, c_backend='louvain', chkpt_save_path=os.getcwd(), random_state=0,
+    cluster_kwargs={}, neighbor_kwargs={}, trainer_kwargs={}, viz_method='umap', viz_kwargs={}, n_neighbors_ti=30, threshold=0.5
+):
+    # Seed setting
+    seed_everything(seed=random_state)
+
+    # In case we need to reference self data using keys
+    ad.obsm['X'] = ad.X
+
+    # Dimensionality reduction and clustering
+    print('Computing embeddings and clusters...')
+    with warnings.catch_warnings():
+        # Filter out user warnings from PyTorch about saving scheduler state
+        warnings.simplefilter("ignore")
+        train_metric_learner(ad, n_episodes=n_episodes, n_metric_epochs=n_metric_epochs, obsm_data_key=use_rep, code_size=code_size,
+            backend=c_backend, save_path=chkpt_save_path, cluster_kwargs=cluster_kwargs, nn_kwargs=neighbor_kwargs, trainer_kwargs=trainer_kwargs
+        )
+
+    # 2d embedding visualization generation
+    print('Computing 2d embedding visualizations...')
+    ad.obsm['metric_viz_embedding'] = generate_plot_embeddings(
+        ad.obsm['X_embedding'], method=viz_method, random_state=random_state, **viz_kwargs
+    )
+
+    # TI
+    print('Computing trajectory graphs...')
+    communities = ad.obs['metric_clusters'].to_numpy().astype(np.int)
+    X = ad.obsm['X_embedding']
+
+    n_neighbors = n_neighbors_ti
+    nbrs = NearestNeighbors(n_neighbors=n_neighbors, metric="euclidean").fit(X)
+    adj_dist = nbrs.kneighbors_graph(X, mode="distance")
+    adj_conn = nbrs.kneighbors_graph(X)
+
+    connectivity, scores = compute_directed_cluster_connectivity(communities, adj_conn, threshold=threshold)
+    un_connectivity, un_scores = compute_undirected_cluster_connectivity(communities, adj_conn, threshold=threshold)
+    
+    ad.uns['metric_directed_connectivities'] = connectivity
+    ad.uns['metric_undirected_connectivities'] = un_connectivity
+    ad.uns['metric_directed_scores'] = scores
+    ad.uns['metric_undirected_scores'] = un_scores
+
+    start_cell_ids = ad.uns['start_id']
+    start_cluster_ids = get_start_cell_cluster_id(ad, start_cell_ids, communities)
+    g, node_positions = compute_trajectory_graph(ad.obsm['metric_viz_embedding'], communities, cluster_connectivities, start_cluster_ids)
+    ad.uns['metric_trajectory'] = g
+    ad.uns['metric_trajectory_node_positions'] = node_positions
+
+    g, node_positions = compute_connectivity_graph(ad.obsm['metric_viz_embedding'], communities, cluster_connectivities, mode='undirected')
+    ad.uns['metric_undirected_graph'] = g
+    ad.uns['metric_undirected_node_positions'] = node_positions
+
+    # Pseudotime computation
+    print('Computing Pseudotime...')
+    trajectory_graph = nx.to_numpy_array(ad.uns['metric_trajectory'])
+    pseudotime = compute_pseudotime(ad, start_cell_ids, adj_conn, adj_dist, trajectory_graph, comm_key='metric_clusters', data_key='X_embedding')

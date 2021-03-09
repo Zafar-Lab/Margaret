2f4c196944c006b6cb0997742e7b3e5865da2184
Kushagra Pandey <kpandeyce008@gmail.com>
2021-03-09 08:24:34 +0530

    Added device to metric learner

--- ensemble-ti/train_metric.py
+++ ensemble-ti/train_metric.py
@@ -14,7 +14,7 @@ from utils.util import compute_runtime, determine_cell_clusters
 
 @compute_runtime
 def train_metric_learner(
-    adata, n_episodes=10, n_metric_epochs=10, code_size=10, obsm_data_key='X_pca',
+    adata, n_episodes=10, n_metric_epochs=10, code_size=10, obsm_data_key='X_pca', device='gpu',
     random_state=0, save_path=os.getcwd(), backend='kmeans', nn_kwargs={}, trainer_kwargs={}, cluster_kwargs={}
 ):
     X = adata.obsm[obsm_data_key]
@@ -37,10 +37,10 @@ def train_metric_learner(
 
     # Model
     infeatures = X.shape[-1]
-    model = MetricEncoder(infeatures, code_size=code_size).cuda()
+    model = MetricEncoder(infeatures, code_size=code_size).to(device)
 
     # Trainer
-    trainer = MetricTrainer(dataset, model, train_loss, random_state=random_state, **trainer_kwargs)
+    trainer = MetricTrainer(dataset, model, train_loss, random_state=random_state, backend=device, **trainer_kwargs)
 
     for episode_idx in range(n_episodes):
         print(f'Training for episode: {episode_idx + 1}')
@@ -54,7 +54,7 @@ def train_metric_learner(
         model.eval()
         with torch.no_grad():
             for data in tqdm(embedding_dataset):
-                data = data.cuda()
+                data = data.to(device)
                 embedding.append(model(data.unsqueeze(0)).squeeze().cpu().numpy())
         X_embedding = np.array(embedding)
 

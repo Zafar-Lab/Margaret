2cb17629131b491253bb2741c1bd111861d6da94
Kushagra Pandey <kpandeyce008@gmail.com>
2021-04-04 12:16:19 +0530

    Bug fixes

--- ensemble-ti/models/ti/pseudotime_v2.py
+++ ensemble-ti/models/ti/pseudotime_v2.py
@@ -17,12 +17,12 @@ def compute_pseudotime(ad, start_cell_ids, adj_dist, adj_cluster, comm_key='metr
         cluster_idx = communities == idx
         clusters.append(cluster_idx)
 
-    ############# Pruning step ##############
+    # Prune the initial adjacency matrix
     adj_dist_pruned = prune_network_edges(communities, adj_dist.todense(), adj_cluster)
     adj_dist_pruned = pd.DataFrame(adj_dist_pruned, index=ad.obs_names, columns=ad.obs_names)
 
-    ############# Pseudotime computation #############
-    start_indices = np.where(ad.obs_names == start_cell_ids)[0]
+    # Pseudotime computation
+    start_indices = [np.where(ad.obs_names == s)[0][0] for s in start_cell_ids]
     p = dijkstra(adj_dist_pruned, indices=start_indices, min_only=True)
     pseudotime = pd.Series(p, index=ad.obs_names)
 
@@ -33,7 +33,7 @@ def compute_pseudotime(ad, start_cell_ids, adj_dist, adj_cluster, comm_key='metr
         adj_sc = connect_graph(adj_sc, data.loc[cluster, :], np.where(adj_sc.index == cluster_start_cell)[0][0])
 
     # Recompute the pseudotime after making the clusters compact
-    p, _ = dijkstra(adj_dist_pruned, indices=start_indices, min_only=True)
+    p = dijkstra(adj_dist_pruned, indices=start_indices, min_only=True)
     pseudotime = pd.Series(p, index=ad.obs_names)
 
     # Add pseudotime to annotated data object

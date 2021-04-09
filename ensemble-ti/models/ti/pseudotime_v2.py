Fixed bug during pseudotime computation

ensemble-ti/models/ti/pseudotime_v2.py
@@ -8,25 +8,25 @@ from utils.util import get_start_cell_cluster_id, compute_runtime, prune_network
 @compute_runtime
 def compute_pseudotime(ad, start_cell_ids, adj_dist, adj_cluster, comm_key='metric_clusters', data_key='metric_embedding'):
     communities = ad.obs[comm_key]
-    n_communities = np.unique(communities).shape[0]
+    cluster_ids = np.unique(communities)
     data = pd.DataFrame(ad.obsm[data_key], index=ad.obs_names)
 
     # Create cluster index
-    clusters = []
-    for idx in range(n_communities):
+    clusters = {}
+    for idx in cluster_ids:
         cluster_idx = communities == idx
-        clusters.append(cluster_idx)
+        clusters[idx] = cluster_idx
 
     # Prune the initial adjacency matrix
-    adj_dist_pruned = prune_network_edges(communities, adj_dist.todense(), adj_cluster)
-    adj_dist_pruned = pd.DataFrame(adj_dist_pruned, index=ad.obs_names, columns=ad.obs_names)
+    adj_dist = pd.DataFrame(adj_dist.todense(), index=ad.obs_names, columns=ad.obs_names)
+    adj_dist_pruned = prune_network_edges(communities, adj_dist, adj_cluster)
 
-    # Pseudotime computation
+    # Pseudotime computation on the pruned graph
     start_indices = [np.where(ad.obs_names == s)[0][0] for s in start_cell_ids]
-    p = dijkstra(adj_dist_pruned, indices=start_indices, min_only=True)
+    p = dijkstra(adj_dist_pruned.to_numpy(), indices=start_indices, min_only=True)
     pseudotime = pd.Series(p, index=ad.obs_names)
 
-    for cluster in clusters:
+    for c_idx, cluster in clusters.items():
         p_cluster = pseudotime.loc[cluster]
         cluster_start_cell = p_cluster.idxmin()
         adj_sc = adj_dist_pruned.loc[cluster, cluster]
@@ -38,6 +38,9 @@ def compute_pseudotime(ad, start_cell_ids, adj_dist, adj_cluster, comm_key='metr
     # Recompute the pseudotime with the updated graph
     p = dijkstra(adj_dist_pruned, indices=start_indices, min_only=True)
     pseudotime = pd.Series(p, index=ad.obs_names)
+
+    # Set the pseudotime for unreachable cells to 0
+    pseudotime[pseudotime == np.inf] = 0
 
     # Add pseudotime to annotated data object
     ad.obs['metric_pseudotime_v2'] = pseudotime

ensemble-ti/utils/util.py
@@ -141,29 +141,30 @@ def get_start_cell_cluster_id(data, start_cell_ids, communities):
 
 
 def prune_network_edges(communities, adj_sc, adj_cluster):
-    n_communities = np.unique(communities).shape[0]
+    cluster_ids = np.unique(communities)
     n_pruned = 0
 
     # Create cluster index
-    clusters = []
-    for idx in range(n_communities):
+    clusters = {}
+    for idx in cluster_ids:
         cluster_idx = communities == idx
-        clusters.append(cluster_idx)
+        clusters[idx] = cluster_idx
 
-    n_row, n_col = adj_cluster.shape
-    col_ids = np.arange(n_col)
-    for c_idx in range(n_row):
+    col_ids = adj_cluster.columns
+    for c_idx in adj_cluster.index:
         cluster_i = clusters[c_idx]
-        non_connected_clusters = col_ids[adj_cluster[c_idx] == 0]
+        non_connected_clusters = col_ids[adj_cluster.loc[c_idx, :] == 0]
         for nc_idx in non_connected_clusters:
             if nc_idx == c_idx:
                 continue
             cluster_nc = clusters[nc_idx]
+            adj_i_nc = adj_sc.loc[cluster_i, cluster_nc]
+
             # Keep track of number of edges pruned for book-keeping!
-            n_pruned += np.sum(adj_sc[cluster_i, :][:, cluster_nc] > 0)
+            n_pruned += np.sum(adj_i_nc.to_numpy() > 0)
 
             # Prune (remove the edges between two non-connected clusters)
-            adj_sc[cluster_i, :][:, cluster_nc] = np.zeros_like(adj_sc[cluster_i, :][:, cluster_nc]).squeeze()
+            adj_sc.loc[cluster_i, cluster_nc] = np.zeros_like(adj_i_nc).squeeze()
 
     print(f'Successfully pruned {n_pruned} edges')
     return adj_sc
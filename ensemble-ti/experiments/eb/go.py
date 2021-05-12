6c49aeb02c839b0f69850446a0dcea4aeae98db8
Kushagra Pandey <kpandeyce008@gmail.com>
2021-05-12 06:55:22 +0530

    Added code to filter go ter,s

--- ensemble-ti/experiments/eb/go.py
+++ ensemble-ti/experiments/eb/go.py
@@ -50,3 +50,26 @@ def generate_go_terms(
             save_path = os.path.join(save_dir, f"GO_{idx}.csv")
             go_df.to_csv(save_path, index=False)
             print(f'GO terms for cluster: {idx} written at: {save_path}')
+
+
+def filter_go_terms(terms_file_path, pat_file_path):
+    # Read patterns and strip newline
+    patterns = []
+    with open(pat_file_path, 'r') as fp:
+        patterns = fp.readlines()
+    patterns = [p.strip('\n') for p in patterns]
+
+    # Read GO terms
+    go_df = pd.read_csv(terms_file_path)
+    go_df.index = go_df['native']
+    index = go_df.index
+    name = go_df['name']
+
+    # Filter
+    unique_ids = set()
+    for pat in patterns:
+        inds = index[name.str.match(pat)]
+        unique_ids = unique_ids.union(set(inds))
+
+    filtered = go_df.loc[unique_ids]
+    return filtered, list(unique_ids)
